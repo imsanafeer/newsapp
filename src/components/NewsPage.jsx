e2d4ee6fa301b6f9a39f4068196d27c09e7fee3e
imsanafeer <imsanafeer@gmail.com>
2024-07-28 14:46:30 +0530

    env

--- src/components/NewsPage.jsx
+++ src/components/NewsPage.jsx
@@ -19,7 +19,7 @@ const NewsPage = ({ category, headline }) => {
       try {
         const response = await fetch(url);
         if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error(`API Limit Reached`);
         }
         const data = await response.json();
         if (data.articles) {
